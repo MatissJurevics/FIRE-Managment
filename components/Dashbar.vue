04413051fc4a256f52f5444d063c7629496ce0c5
matiss <matissjurevics@gmail.com>
2022-06-05 16:52:46 +0100

    dashnav looks good

---
+++ components/Dashbar.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+  <nav>
+
+    <v-app-bar
+      absolute
+      color="#CF1259"
+      dark
+
+    >
+
+
+
+
+      <v-app-bar-title>Title</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Summary</v-tab>
+          <v-tab>Budgets</v-tab>
+          <v-tab>Expenditure</v-tab>
+          <v-tab>Investments</v-tab>
+          <v-tab>Calculators</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+
+  </nav>
+</template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,17 @@
 <script lang="ts">
-export default {
-
-}
+export default {};
 </script>
 
 <template>
-    <nav>
-        <p>Navbar</p>
-    </nav>
+  <nav>
+    <v-app-bar color="#bbb" dark elevate-on-scroll>
+      <v-toolbar-title>Finance</v-toolbar-title>
+      <v-spacer />
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-app-bar-nav-icon class="hidden"> </v-app-bar-nav-icon>
+    </v-app-bar>
+  </nav>
 </template>
